eda27ed456deb81c8aa799c98969a7169b344f0a
yuriy-smplt <yuriy.p@smplt.net>
2023-03-29 12:19:46 +0300

    fix

--- src/MyJetWallet.Clearjunction.Client/ClearJunctionClient.Wallet.cs
+++ src/MyJetWallet.Clearjunction.Client/ClearJunctionClient.Wallet.cs
@@ -4,23 +4,24 @@ using System.Threading.Tasks;
 using MyJetWallet.ClearJunction.Models;
 using MyJetWallet.ClearJunction.Models.RequisitesAllocation;
 using MyJetWallet.ClearJunction.Models.Statements;
+using MyJetWallet.ClearJunction.Models.Wallet;
 
 namespace MyJetWallet.ClearJunction
 {
     public partial class ClearJunctionClient
     {
         #region Wallet
-        public async Task<WebCallResult<IbanInformation>> GetWalletAsync(string wallet, bool returnPaymentMethods,
+        public async Task<WebCallResult<Wallet>> GetWalletAsync(string wallet, bool returnPaymentMethods = false,
             CancellationToken cancellationToken = default)
         {
             if (returnPaymentMethods)
             {
-                return await GetAsync<IbanInformation>(
+                return await GetAsync<Wallet>(
                     $"v7/bank/wallets/{wallet}?returnPaymentMethods=true",
                     cancellationToken);
             }
 
-            return await GetAsync<IbanInformation>(
+            return await GetAsync<Wallet>(
                 $"v7/bank/wallets/{wallet}",
                 cancellationToken);
         }
