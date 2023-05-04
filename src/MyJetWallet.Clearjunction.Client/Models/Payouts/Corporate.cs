1e6081c797558c40fa13e787994a677d0853e667
yuriy-smplt <yuriy.p@smplt.net>
2023-05-04 16:39:24 +0300

    updated Corporate

--- src/MyJetWallet.Clearjunction.Client/Models/Payouts/Corporate.cs
+++ src/MyJetWallet.Clearjunction.Client/Models/Payouts/Corporate.cs
@@ -8,21 +8,21 @@ namespace MyJetWallet.ClearJunction.Models.Payouts;
 [DataContract]
 public class Corporate
 {
-    [DataMember(Order = 1), JsonProperty("email")]
+    [DataMember(Order = 1), JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
     public string Email { get; set; }
 
     [DataMember(Order = 2), JsonProperty("name")]
     public string Name { get; set; }
 
-    [DataMember(Order = 3), JsonProperty("registrationNumber")]
+    [DataMember(Order = 3), JsonProperty("registrationNumber", NullValueHandling = NullValueHandling.Ignore)]
     public string RegistrationNumber { get; set; }
 
-    [DataMember(Order = 4), JsonProperty("incorporationCountry")]
+    [DataMember(Order = 4), JsonProperty("incorporationCountry", NullValueHandling = NullValueHandling.Ignore)]
     public string IncorporationCountry { get; set; }
 
     [DataMember(Order = 5), JsonProperty("address")]
     public Address Address { get; set; }
 
-    [DataMember(Order = 6), JsonProperty("incorporationDate"), JsonConverter(typeof(CustomDateTimeConverter))]
+    [DataMember(Order = 6), JsonProperty("incorporationDate", NullValueHandling = NullValueHandling.Ignore ), JsonConverter(typeof(CustomDateTimeConverter))]
     public DateTime IncorporationDate { get; set; }
 }
