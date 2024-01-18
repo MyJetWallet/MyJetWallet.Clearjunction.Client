488899a6cf0fbc4ec2d2600c08975f307996fea0
hlib-oreshchuk <hlib.o@simple.app>
2024-01-18 02:52:47 +0200

    fix naming

--- src/MyJetWallet.Clearjunction.Client/Models/Reports/GetReportRequest.cs
+++ src/MyJetWallet.Clearjunction.Client/Models/Reports/GetReportRequest.cs
@@ -9,9 +9,9 @@ public class GetReportRequest
     [DataMember(Order = 1), JsonProperty("walletUuid")]
     public string WalletUuid { get; set; }
 
-    [DataMember(Order = 2), JsonProperty("dateFrom")]
+    [DataMember(Order = 2), JsonProperty("timestampFrom")]
     public string DateFrom { get; set; }
 
-    [DataMember(Order = 3), JsonProperty("dateTo")]
+    [DataMember(Order = 3), JsonProperty("timestampTo")]
     public string DateTo { get; set; }
 }
